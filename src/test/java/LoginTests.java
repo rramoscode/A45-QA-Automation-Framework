20e488920ff21ae83189a9e7a852732f395a7ce8
rramoscode <randy.ramos@testpro.io>
2023-04-20 12:33:46 -0500

    Homework16 Test Registration Navigation
    -Create a new file, Homework16.java in IntelliJ IDEA.
    -Create a test case, registrationNavigation() using @Test annotation.
    -Using Selenium, navigate to "https://bbb.testpro.io/".
    -Click the Registration link.
    -Verify that you are redirected to the Registration page using the Assert method.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,6 +9,36 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+@Test
+    public static void successfulLoginTest() {
+
+//      Added ChromeOptions argument below to fix websocket error
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.sendKeys("randy.ramos@testpro.io");
+
+
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.sendKeys("te$t$tudent");
+
+
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+// expected Result
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+        driver.quit();
+
+    }
     @Test
     public static void LoginEmptyEmailPasswordTest() {
 
