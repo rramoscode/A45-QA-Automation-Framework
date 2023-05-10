380a700a2a59753f622e555be71c88585314fb41
rramoscode <randy.ramos@testpro.io>
2023-05-09 21:51:18 -0500

    homework24 using standalone option locally
    - Set up  Selenium Grid (Standalone option) locally. Chrome/ firefox browser etc..
    - Implement Browser Factory method as we had in the class, so I could run Chrome browser using your local Grid.
    
    test changes rrPlaylist3 to rrPlaylist4

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,13 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
@@ -23,18 +31,24 @@ public class BaseTest {
 // Test annotation and the helper/reusable methods
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+
+//        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.firefoxdriver().setup();
     }
     @BeforeMethod
     @Parameters ({"Hw19BaseURL"})
 
-    public void setUpBrowser (String Hw19BaseURL) {
+    public void setUpBrowser (String Hw19BaseURL) throws MalformedURLException {
 //     Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
 
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
+//        driver = new ChromeDriver(options);
+//        driver =  new FirefoxDriver();
+
+// myBrowser method for Pbrowser parameter for variable browser
+        driver = pickBrowser(System.getProperty("browser"));
+
 //       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 // instance - initializes the wait variable with a new WebDriverWait object that will wait up to 10 seconds for the expected condition to be met.
@@ -47,6 +61,39 @@ public class BaseTest {
     @AfterMethod
     public void tearDownBrowser () {
         driver.quit();
+    }
+// implementing Browser Functionality when setting parameter in gradle
+    private static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURl = "http://192.168.1.22:4444";
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+            case "grid-firefox":
+                caps.setCapability("browserName" , "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURl).toURL(),caps);
+            case "grid-edge":
+                caps.setCapability("browserName" , "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURl).toURL(),caps);
+            case "grid-safari":
+                caps.setCapability("browserName" , "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURl).toURL(),caps);
+            case "grid-chrome":
+                caps.setCapability("browserName" , "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURl).toURL(),caps);
+            default:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                WebDriverManager.chromedriver().setup();
+                return driver = new ChromeDriver(options);
+        }
     }
 
     public void openloginUrl() {
---
+++ src/test/java/Homework24.java
@@ -0,0 +1,20 @@
+import PageFactory.HomePage;
+import PageFactory.LoginPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+
+public class Homework24 extends BaseTest {
+
+    @Test
+    public void renamePlaylist() {
+
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+
+        loginPage.login();
+        homePage.doubleClickSelectPlaylist();
+        homePage.doubleClickUpdatePlaylistName();
+
+    }
+}
